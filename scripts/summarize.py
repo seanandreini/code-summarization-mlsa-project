e49932082338c56a9fc279061efd5c1bfec9d580
Sean <sean.andreini@gmail.com>
2026-02-10 18:29:17 +0100

    fix summarize

--- scripts/summarize.py
+++ scripts/summarize.py
@@ -2,18 +2,19 @@ import argparse
 import torch
 from transformers import PreTrainedTokenizerFast
 from gensim.corpora import Dictionary
+import os
 
 from src.utils import load_checkpoint, decode_ids_to_docstring, encode_code_to_ids
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument('--checkpoint_dir', required=True, type=str)
+  parser.add_argument('--dir', required=True, type=str)
   parser.add_argument('--exp_name', required=True, type=str)
-  parser.add_argument('--code', required=True, type=str)
+  parser.add_argument('--input', required=True, type=str)
   args = parser.parse_args()
 
   config = {}
-  config['checkpoint_dir'] = args.checkpoint_dir
+  config['checkpoint_dir'] = args.dir
   config['exp_name'] = args.exp_name
   config['device'] = 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
 
@@ -24,10 +25,10 @@ def main():
   else:
     from src.transformer import predict_ids
 
-  src_dictionary = Dictionary.load(config['processed_dataset_path']+'code_dictionary.pt')
-  pred_ids = predict_ids(model, encode_code_to_ids(args.code, src_dictionary), config)
+  src_dictionary = Dictionary.load(os.path.join(config['processed_dataset_path']+'code_dictionary.pt'))
+  pred_ids = predict_ids(model, encode_code_to_ids(args.input, src_dictionary), config)
 
-  tgt_tokenizer = PreTrainedTokenizerFast(tokenizer_file=config['processed_dataset_path']+'bpe_tokenizer.json',
+  tgt_tokenizer = PreTrainedTokenizerFast(tokenizer_file=os.path.join(config['processed_dataset_path']+'bpe_tokenizer.json'),
 																			pad_token="[PAD]",
 																			bos_token="[BOS]",
 																			eos_token="[EOS]",
